1a183bde6de1a00827bb465a7737c969003a1438
Mumuuh6 <meherafrinmridula@gmai.com>
2025-08-22 01:18:13 +0600

    dashboard and others

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,14 +3,13 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { WeatherDashboard } from './weather-dashboard'
 
 function Home() {
   //const [count, setCount] = useState(0)
 
   return (
-    <div className='max-w-6xl mx-auto mt-4'>
-      j
-    </div>
+    <WeatherDashboard></WeatherDashboard>
   )
 }
 
